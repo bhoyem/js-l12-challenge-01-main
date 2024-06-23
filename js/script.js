2fed6f6b9dec75c2f9a3b1cbbdceb26764d6e8bb
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-06-23 09:58:16 -0500

    Added function to display a retrieved image

--- js/script.js
+++ js/script.js
@@ -3,10 +3,28 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-const getImage = async function(){
+const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+    selectRandomImage(images);
+    // console.log();
 }
 
-getImage();
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    displayImage(randomImage);
+    // console.log(randomImage);
+};
+
+displayImage = function (randomImage) {
+    let author = randomImage.author;
+    let imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+}
+
+button.addEventListener("click", function(){
+    getImage();
+});
